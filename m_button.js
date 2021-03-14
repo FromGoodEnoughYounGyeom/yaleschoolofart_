0af8a271423c8b6ece69173d3264938cf034baa2
FromGoodEnoughYounGyeom <webdjanelee@gmail.com>
2021-03-14 16:03:33 +0900

    add: media query toggle button

---
+++ m_button.js
@@ -0,0 +1,14 @@
+
+
+
+function menuToggle() {
+	var menu = document.querySelector(".menu");
+	if (menu.style.display === "block") {
+		menu.style.display = "none";
+	} else {
+		menu.style.display = "block";
+	}
+  }
+
+
+
